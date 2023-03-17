6cd6648a8e459b6bbfb1746f5c5274ced65e1bcd
silviacasavola <silcasavola@gmail.com>
2023-03-17 12:14:26 +0100

    Update global.js

--- global.js
+++ global.js
@@ -2,7 +2,7 @@
 
 $("main").scroll(function () {
 
-  if ($(window).width() > 991) {
+  if ($(window).width() >= 600) {
 if ($("main").scrollTop() == 0) {
   // $("#hidden-header").css("top", "-3.7vw");
   $("header").css("background-color", "rgba(191, 46, 46, 0)")
