f069f13fd22ad990e5ccac42e969f550b858847f
silviacasavola <silcasavola@gmail.com>
2023-03-17 18:40:02 +0100

    Update global.js

--- global.js
+++ global.js
@@ -31,18 +31,8 @@ $("main").scroll(function () {
 
 
 // SAFE AREA
-// Get the safe area insets for the current viewport
-// const { safeAreaInsets } = window.visualViewport;
-
-// Set the safe area insets as CSS variables
-document.documentElement.style.setProperty('--safe-area-inset-top', `${safeAreaInsets.top}px`);
-// document.documentElement.style.setProperty('--safe-area-inset-right', `${safeAreaInsets.right}px`);
-document.documentElement.style.setProperty('--safe-area-inset-bottom', `${safeAreaInsets.bottom}px`);
-// document.documentElement.style.setProperty('--safe-area-inset-left', `${safeAreaInsets.left}px`);
-
-// In this example, we're using the setProperty() method to set CSS variables on the document.documentElement element. We're using the safe area insets retrieved from the visualViewport object to set the values of these variables.
-// Once you've set these variables, you can use them in your CSS as you would any other variable, like this:
-// .my-element {
-//   position: absolute;
-//   bottom: var(--safe-area-inset-bottom);
+if (window.visualViewport && window.visualViewport.safeAreaInsets) {
+  const { safeAreaInsets } = window.visualViewport;
+  document.documentElement.style.setProperty('--safe-area-inset-top', `${safeAreaInsets.top}px`);
+  document.documentElement.style.setProperty('--safe-area-inset-bottom', `${safeAreaInsets.bottom}px`);
 }
