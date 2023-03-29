9cf1c39b18d2bf15a4c4e306b063cf06ec5b10b5
silviacasavola <silcasavola@gmail.com>
2023-03-29 09:06:45 +0200

    ok

--- global.js
+++ global.js
@@ -16,8 +16,6 @@ if ($("main").scrollTop() == 0) {
   $("header").css("background-color", "rgba(191, 46, 46, 1)")
 }
 }
-
-safearea()
 })
 
 
@@ -33,45 +31,3 @@ $("main").scroll(function() {closemenu()})
 function closemenu() {
   $("header").addClass("menu-closed");
 }
-
-
-// SAFE AREA
-
-$(document).ready(function() {safearea()})
-$( window ).resize(function() {safearea()})
-
-function safearea() {
-
-    if ($(window).width() <= 575.8 && $("body").scrollTop() <= 10 ) {
-
-hamburgerPos = $("#hamburger-icon").position();
-let safeArea = hamburgerPos.top;
-// document.documentElement.style.setProperty('--safe-area', safeArea);
-$("#sec1-link").css("top", safeArea)
-} else {
-  $("#sec1-link").css("top", "unset")
-}
-}
-
-
-
-
-// FAIL: SAFE AREA
-// let safeAreaInsets;
-
-// if (window.visualViewport && window.visualViewport.safeAreaInsets) {
-// ({ safeAreaInsets } = window.visualViewport);
-//   document.documentElement.style.setProperty('--safe-area-inset-top', `${safeAreaInsets.top}px`);
-//   document.documentElement.style.setProperty('--safe-area-inset-bottom', `${safeAreaInsets.bottom}px`);
-// $("#scrittone").html({ safeAreaInsets })
-// }
-
-// let altra = getComputedStyle(document.documentElement).getPropertyValue("--sab");
-// $("#scrittone2").html(altra)
-
-// let hamburgerPos = $("#hamburger-icon").position();
-// let margin = $("#hamburger-icon").css("margin");
-// let safeArea = $(window).height() - hamburgerPos.top - $("#hamburger-icon").height() - margin*2;
-// document.documentElement.style.setProperty('--safe-area-bottom', safeArea);
-// $("#scrittone").html(safeArea)
-// $("#sec1-link").css("bottom", safeArea)
